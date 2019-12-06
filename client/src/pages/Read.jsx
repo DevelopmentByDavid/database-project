59f453cc162b288a09a487b38ca35d71483a5c3d
Michael Bradley <mbrad94@gmail.com>
2019-12-06 10:22:59 -0800

    Add forms to accessible pages

--- client/src/pages/Read.jsx
+++ client/src/pages/Read.jsx
@@ -5,12 +5,22 @@ import FormRoomsAvailable from '../components/FormRoomsAvailable';
 import FormRoomsBooked from '../components/FormRoomsBooked';
 import FormBookingsForWeek from '../components/FormBookingsForWeek';
 import FormHighestPricedRooms from '../components/FormHighestPricedRooms';
+import FormTopBooking from '../components/FormTopBooking';
+import FormCustomerCost from '../components/FormCustomerCost';
+import FormRepairsByCompany from '../components/FormRepairsByCompany';
+import FormTopMaintComp from '../components/FormTopMaintComp';
+import FormRepairsPerYear from '../components/FormRepairsPerYear';
 
 const config = {
     '7': <FormRoomsAvailable />,
     '8': <FormRoomsBooked />,
     '9': <FormBookingsForWeek />,
-    '10': <FormHighestPricedRooms />
+    '10': <FormHighestPricedRooms />,
+    '11': <FormTopBooking />,
+    '12': <FormCustomerCost />,
+    '13': <FormRepairsByCompany />,
+    '14': <FormTopMaintComp />,
+    '15': <FormRepairsPerYear />
 };
 
 export default function Create() {
