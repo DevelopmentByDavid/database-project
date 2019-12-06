fde83738232caa04c09f9053902bdf124f904284
Michael Bradley <mbrad94@gmail.com>
2019-12-06 13:06:16 -0800

    Connect FormTopBooking to server

--- client/src/components/FormTopBooking.jsx
+++ client/src/components/FormTopBooking.jsx
@@ -4,6 +4,8 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Dialog from './Dialog';
+import {formatQuery} from '../lib/format';
 
 const fields = [
     'customerID',
@@ -15,6 +17,7 @@ const labels = [
 ];
 
 export default function FormRepairsPerYear() {
+    const [data, setData] = React.useState(null);
     const handleSubmit = event => {
         event.preventDefault();
         const {
@@ -25,35 +28,53 @@ export default function FormRepairsPerYear() {
             customerID: customerID.value,
             outputLimit: outputLimit.value
         };
-        console.log(values);
-        console.log('TODO');
+        const url = formatQuery('/read/11', values);
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                res.json().then(({ data: resData }) => {
+                    // console.log(obj);
+                    console.log(resData);
+                    setData(resData);
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
     };
     return (
-        <Paper style={{ padding: '16px' }}>
-            <Typography variant='h4'>Top Booking Prices</Typography>
-            <form id='new-customer' onSubmit={handleSubmit}>
-                <Grid container spacing={2}>
-                    {fields.map((fieldName, idx) => (
-                        <Grid item xs={12} key={fieldName}>
-                            <TextField
-                                // key={fieldName}
-                                id={fieldName}
-                                label={labels[idx]}
-                                fullWidth
-                            />
+        <>
+            <Paper style={{ padding: '16px' }}>
+                <Typography variant='h4'>Top Booking Prices</Typography>
+                <form id='new-customer' onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {fields.map((fieldName, idx) => (
+                            <Grid item xs={12} key={fieldName}>
+                                <TextField
+                                    // key={fieldName}
+                                    id={fieldName}
+                                    label={labels[idx]}
+                                    fullWidth
+                                />
+                            </Grid>
+                        ))}
+                        <Grid container item xs={12} justify='flex-end'>
+                            <Button
+                                variant='contained'
+                                onSubmit={handleSubmit}
+                                type='submit'
+                            >
+                                Submit
+                            </Button>
                         </Grid>
-                    ))}
-                    <Grid container item xs={12} justify='flex-end'>
-                        <Button
-                            variant='contained'
-                            onSubmit={handleSubmit}
-                            type='submit'
-                        >
-                            Submit
-                        </Button>
                     </Grid>
-                </Grid>
-            </form>
-        </Paper>
+                </form>
+            </Paper>
+            <Dialog data={data || []} open={Boolean(data)} handleClose={() => setData(null)} />
+        </>
     );
 }
